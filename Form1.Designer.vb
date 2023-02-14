e746f9739a90dc83706d33a63fdd8d2c5ddaa406
Skyler <sanderson2737@hotmail.com>
2023-02-13 22:38:26 -0500

    v0.0.1

---
+++ Form1.Designer.vb
@@ -0,0 +1,190 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class MainForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.txtJimSales = New System.Windows.Forms.TextBox()
+        Me.txtDaveSales = New System.Windows.Forms.TextBox()
+        Me.txtRobSales = New System.Windows.Forms.TextBox()
+        Me.txtJimPerc = New System.Windows.Forms.TextBox()
+        Me.txtDavePerc = New System.Windows.Forms.TextBox()
+        Me.txtRobPerc = New System.Windows.Forms.TextBox()
+        Me.btnCalc = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(40, 121)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(43, 25)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Jim"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(40, 198)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(58, 25)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Dave"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(40, 281)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(47, 25)
+        Me.Label3.TabIndex = 1
+        Me.Label3.Text = "Rob"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(182, 55)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(111, 25)
+        Me.Label4.TabIndex = 1
+        Me.Label4.Text = "Total Sales"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(370, 55)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(122, 25)
+        Me.Label5.TabIndex = 1
+        Me.Label5.Text = "Percentages"
+        '
+        'txtJimSales
+        '
+        Me.txtJimSales.Location = New System.Drawing.Point(187, 121)
+        Me.txtJimSales.Name = "txtJimSales"
+        Me.txtJimSales.Size = New System.Drawing.Size(106, 22)
+        Me.txtJimSales.TabIndex = 1
+        '
+        'txtDaveSales
+        '
+        Me.txtDaveSales.Location = New System.Drawing.Point(187, 198)
+        Me.txtDaveSales.Name = "txtDaveSales"
+        Me.txtDaveSales.Size = New System.Drawing.Size(106, 22)
+        Me.txtDaveSales.TabIndex = 2
+        '
+        'txtRobSales
+        '
+        Me.txtRobSales.Location = New System.Drawing.Point(187, 281)
+        Me.txtRobSales.Name = "txtRobSales"
+        Me.txtRobSales.Size = New System.Drawing.Size(106, 22)
+        Me.txtRobSales.TabIndex = 3
+        '
+        'txtJimPerc
+        '
+        Me.txtJimPerc.Location = New System.Drawing.Point(375, 125)
+        Me.txtJimPerc.Name = "txtJimPerc"
+        Me.txtJimPerc.ReadOnly = True
+        Me.txtJimPerc.Size = New System.Drawing.Size(117, 22)
+        Me.txtJimPerc.TabIndex = 10
+        '
+        'txtDavePerc
+        '
+        Me.txtDavePerc.Location = New System.Drawing.Point(375, 202)
+        Me.txtDavePerc.Name = "txtDavePerc"
+        Me.txtDavePerc.ReadOnly = True
+        Me.txtDavePerc.Size = New System.Drawing.Size(117, 22)
+        Me.txtDavePerc.TabIndex = 10
+        '
+        'txtRobPerc
+        '
+        Me.txtRobPerc.Location = New System.Drawing.Point(375, 281)
+        Me.txtRobPerc.Name = "txtRobPerc"
+        Me.txtRobPerc.ReadOnly = True
+        Me.txtRobPerc.Size = New System.Drawing.Size(117, 22)
+        Me.txtRobPerc.TabIndex = 10
+        '
+        'btnCalc
+        '
+        Me.btnCalc.Location = New System.Drawing.Point(432, 338)
+        Me.btnCalc.Name = "btnCalc"
+        Me.btnCalc.Size = New System.Drawing.Size(96, 37)
+        Me.btnCalc.TabIndex = 4
+        Me.btnCalc.Text = "Calculate"
+        Me.btnCalc.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(534, 338)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(96, 37)
+        Me.btnExit.TabIndex = 5
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'MainForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(648, 390)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnCalc)
+        Me.Controls.Add(Me.txtRobPerc)
+        Me.Controls.Add(Me.txtRobSales)
+        Me.Controls.Add(Me.txtDavePerc)
+        Me.Controls.Add(Me.txtDaveSales)
+        Me.Controls.Add(Me.txtJimPerc)
+        Me.Controls.Add(Me.txtJimSales)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "MainForm"
+        Me.Text = "Sales Calculator"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents txtJimSales As TextBox
+    Friend WithEvents txtDaveSales As TextBox
+    Friend WithEvents txtRobSales As TextBox
+    Friend WithEvents txtJimPerc As TextBox
+    Friend WithEvents txtDavePerc As TextBox
+    Friend WithEvents txtRobPerc As TextBox
+    Friend WithEvents btnCalc As Button
+    Friend WithEvents btnExit As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,141 @@
+﻿Public Class MainForm
+
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+
+    Private Sub btnCalc_Click_1(sender As Object, e As EventArgs) Handles btnCalc.Click
+        ' Clear the previous percentages
+        txtJimPerc.Text = ""
+        txtDavePerc.Text = ""
+        txtRobPerc.Text = ""
+        txtJimSales.BackColor = SystemColors.Window
+        txtJimPerc.BackColor = SystemColors.Window
+        txtDaveSales.BackColor = SystemColors.Window
+        txtDavePerc.BackColor = SystemColors.Window
+        txtRobPerc.BackColor = SystemColors.Window
+
+        ' Declare variables to store sales for each salesperson
+        Dim jimSales As Double = 0
+        Dim daveSales As Double = 0
+        Dim robSales As Double = 0
+        Dim totalSales As Double = 0
+        Dim highestPercentage As Double = 0
+        Dim highestSalesperson As String = String.Empty
+        Dim tiedPercentage As Boolean = False
+
+        ' Get sales input from text boxes
+        Double.TryParse(txtJimSales.Text, jimSales)
+        Double.TryParse(txtDaveSales.Text, daveSales)
+        Double.TryParse(txtRobSales.Text, robSales)
+
+        ' Calculate total sales
+        totalSales = jimSales + daveSales + robSales
+
+        ' Check for division by zero
+        If totalSales <> 0 Then
+            ' Calculate and display percentages for each salesperson
+            If jimSales > 0 Then
+                txtJimPerc.Text = (jimSales / totalSales * 100).ToString("P1")
+            Else
+                txtJimPerc.Text = "0.0%"
+            End If
+            If daveSales > 0 Then
+                txtDavePerc.Text = (daveSales / totalSales * 100).ToString("P1")
+            Else
+                txtDavePerc.Text = "0.0%"
+            End If
+            If robSales > 0 Then
+                txtRobPerc.Text = (robSales / totalSales * 100).ToString("P1")
+            Else
+                txtRobPerc.Text = "0.0%"
+            End If
+        Else
+            txtJimPerc.Text = "0.0%"
+            txtDavePerc.Text = "0.0%"
+            txtRobPerc.Text = "0.0%"
+        End If
+
+        ' Check if there is a tie in sales
+        If jimSales / totalSales * 100 = daveSales / totalSales * 100 AndAlso jimSales > 0 AndAlso daveSales > 0 Then
+            tiedPercentage = True
+            txtJimSales.BackColor = Color.Yellow
+            txtJimPerc.BackColor = Color.Yellow
+            txtDaveSales.BackColor = Color.Yellow
+            txtDavePerc.BackColor = Color.Yellow
+        ElseIf daveSales / totalSales * 100 = robSales / totalSales * 100 AndAlso daveSales > 0 AndAlso robSales > 0 Then
+            tiedPercentage = True
+            txtDaveSales.BackColor = Color.Yellow
+            txtDavePerc.BackColor = Color.Yellow
+            txtRobSales.BackColor = Color.Yellow
+            txtRobPerc.BackColor = Color.Yellow
+        ElseIf jimSales / totalSales * 100 = robSales / totalSales * 100 AndAlso jimSales > 0 AndAlso robSales > 0 Then
+            tiedPercentage = True
+            txtJimSales.BackColor = Color.Yellow
+            txtJimPerc.BackColor = Color.Yellow
+            txtRobSales.BackColor = Color.Yellow
+            txtRobPerc.BackColor = Color.Yellow
+
+            'If no sales figures are equal, Then the code sets the color Of the text boxes And percentage labels To their Default color
+
+        Else
+            tiedPercentage = False
+            txtJimSales.BackColor = SystemColors.Control
+            txtJimPerc.BackColor = SystemColors.Control
+            txtDaveSales.BackColor = SystemColors.Control
+            txtDavePerc.BackColor = SystemColors.Control
+            txtRobSales.BackColor = SystemColors.Control
+            txtRobPerc.BackColor = SystemColors.Control
+        End If
+
+        ' Calculate and display percentages for each salesperson
+        If jimSales > 0 Then
+            txtJimPerc.Text = (jimSales / totalSales * 100).ToString("P1")
+        Else
+            txtJimPerc.Text = "0.0%"
+        End If
+        If daveSales > 0 Then
+            txtDavePerc.Text = (daveSales / totalSales * 100).ToString("P1")
+        Else
+            txtDavePerc.Text = "0.0%"
+        End If
+        If robSales > 0 Then
+            txtRobPerc.Text = (robSales / totalSales * 100).ToString("P1")
+        Else
+            txtRobPerc.Text = "0.0%"
+        End If
+
+        ' Find the highest salesperson
+        If jimSales > daveSales AndAlso jimSales > robSales Then
+            highestPercentage = jimSales / totalSales * 100
+            highestSalesperson = "Jim"
+        ElseIf daveSales > jimSales AndAlso daveSales > robSales Then
+            highestPercentage = daveSales / totalSales * 100
+            highestSalesperson = "Dave"
+        ElseIf robSales > jimSales AndAlso robSales > daveSales Then
+            highestPercentage = robSales / totalSales * 100
+            highestSalesperson = "Rob"
+        End If
+
+        ' Highlight the highest salesperson
+        If highestSalesperson = "Jim" Then
+            txtJimSales.BackColor = Color.Yellow
+            txtJimPerc.BackColor = Color.Yellow
+        ElseIf highestSalesperson = "Dave" Then
+            txtDaveSales.BackColor = Color.Yellow
+            txtDavePerc.BackColor = Color.Yellow
+        ElseIf highestSalesperson = "Rob" Then
+            txtRobSales.BackColor = Color.Yellow
+            txtRobPerc.BackColor = Color.Yellow
+        End If
+
+    End Sub
+
+End Class
+
+
+
+
+
+
